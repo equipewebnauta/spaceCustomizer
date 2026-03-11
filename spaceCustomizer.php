59856da2e53d33997782b0f61484766c282cc5fc
equipewebnauta <equipewebnauta@gmail.com>
2026-03-11 16:11:05 -0300

    Update spaceCustomizer.php

--- spaceCustomizer.php
+++ spaceCustomizer.php
@@ -1,10 +1,3 @@
-
-
 add_action('wp_footer', function () {
-    ?>
-    <script>
-        console.log('cheguei aqui');
-    </script>
-    <?php
-
+    echo '<script>console.log("cheguei aqui");</script>';
 });
